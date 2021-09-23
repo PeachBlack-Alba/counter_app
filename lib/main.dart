7861867ec5d504100eccf2673bfef4e2c6f2e69e
PeachBlack-Alba <57803942+PeachBlack-Alba@users.noreply.github.com>
2021-09-23 15:38:44 +0200

    course

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
-import 'package:counter_app/cubit/counter_cubit.dart';
+import 'package:counter_app/presentation/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'logic/cubit/counter_cubit.dart';
 
 void main() {
   final CounterState counterState1 = CounterState(counterValue: 1);
@@ -20,135 +22,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body:
-      // This is how we would use the BlocListener, but there is an easier way to do it
-      // With the BlocConsumer -> Combines BlocBuilder and BlocListener in one
-//      BlocListener<CounterCubit, CounterState>(
-//        listener: (context, state) {
-//          if (state.wasIncremented == true) {
-//            Scaffold.of(context).showSnackBar(const SnackBar(
-//              content: Text('Incremented'),
-//              duration: Duration(milliseconds: 300),
-//            ));
-//          } else if (state.wasIncremented == false) {
-//            Scaffold.of(context).showSnackBar(const SnackBar(
-//              content: Text('Decremented'),
-//              duration: Duration(milliseconds: 300),
-//            ));
-//          }
-//        },
-//        child:
-      Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            // We wrap it in here because is the UI that we wants to change when states is changed
-            // Wrapping the whole screen would be a bad practice because we would be rebuilding the whole screen for just one number
-            BlocConsumer<CounterCubit, CounterState>(
-              listener: (context, state) {
-                if (state.wasIncremented == true) {
-                  Scaffold.of(context).showSnackBar(const SnackBar(
-                    content: Text('Incremented'),
-                    duration: Duration(milliseconds: 300),
-                  ));
-                } else if (state.wasIncremented == false) {
-                  Scaffold.of(context).showSnackBar(const SnackBar(
-                    content: Text('Decremented'),
-                    duration: Duration(milliseconds: 300),
-                  ));
-                }
-              },
-              builder: (context, state) {
-                if (state.counterValue < 0) {
-                  return Text(
-                    'Negative value' + state.counterValue.toString(),
-                    style: Theme
-                        .of(context)
-                        .textTheme
-                        .headline4,
-                  );
-                } else if (state.counterValue % 2 == 0) {
-                  return Text(
-                    'Value is even' + state.counterValue.toString(),
-                    style: Theme
-                        .of(context)
-                        .textTheme
-                        .headline4,
-                  );
-                }
-                return Text(
-                  state.counterValue.toString(),
-                  style: Theme
-                      .of(context)
-                      .textTheme
-                      .headline4,
-                );
-              },
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                FloatingActionButton(
-                  onPressed: () {
-                    // we need to call the instance of the cubit:
-                    // two ways of doing it:
-                    // BlocProvider.of<CounterCubit>(context).decrement;
-                    // context.bloc<CounterCubit>().decrement();
-                    // Now that we build the BlocProvider we need to change the UI and show how many times
-                    // We pressed the button, for that, we need the BlocBuilder!
-                    // The BlocBuilder is a widget that helps re-building the UI based on the bloc State Changes
-
-                    BlocProvider
-                        .of<CounterCubit>(context)
-                        .decrement;
-                  },
-                  tooltip: 'Decrement',
-                  child: Icon(Icons.remove),
-                ),
-                FloatingActionButton(
-                  onPressed: () {
-                    BlocProvider.of<CounterCubit>(context).increment();
-                  },
-                  tooltip: 'Increment',
-                  child: Icon(Icons.add),
-                )
-              ],
-            )
-          ],
-        ),
+        home: const HomeScreen(title: 'Flutter Demo Home Page', color: Colors.amberAccent,),
       ),
     );
   }
---
+++ lib/presentation/screens/second_screen.dart
@@ -0,0 +1,131 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:counter_app/logic/cubit/counter_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SecondScreen extends StatefulWidget {
+  const SecondScreen({Key? key, required this.title, required this.color}) : super(key: key);
+
+  final String title;
+  final Color color;
+
+  @override
+  State<SecondScreen> createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body:
+      // This is how we would use the BlocListener, but there is an easier way to do it
+      // With the BlocConsumer -> Combines BlocBuilder and BlocListener in one
+//      BlocListener<CounterCubit, CounterState>(
+//        listener: (context, state) {
+//          if (state.wasIncremented == true) {
+//            Scaffold.of(context).showSnackBar(const SnackBar(
+//              content: Text('Incremented'),
+//              duration: Duration(milliseconds: 300),
+//            ));
+//          } else if (state.wasIncremented == false) {
+//            Scaffold.of(context).showSnackBar(const SnackBar(
+//              content: Text('Decremented'),
+//              duration: Duration(milliseconds: 300),
+//            ));
+//          }
+//        },
+//        child:
+      Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            // We wrap it in here because is the UI that we wants to change when states is changed
+            // Wrapping the whole screen would be a bad practice because we would be rebuilding the whole screen for just one number
+            BlocConsumer<CounterCubit, CounterState>(
+              listener: (context, state) {
+                if (state.wasIncremented == true) {
+                  Scaffold.of(context).showSnackBar(const SnackBar(
+                    content: Text('Incremented'),
+                    duration: Duration(milliseconds: 300),
+                  ));
+                } else if (state.wasIncremented == false) {
+                  Scaffold.of(context).showSnackBar(const SnackBar(
+                    content: Text('Decremented'),
+                    duration: Duration(milliseconds: 300),
+                  ));
+                }
+              },
+              builder: (context, state) {
+                if (state.counterValue < 0) {
+                  return Text(
+                    'Negative value' + state.counterValue.toString(),
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                } else if (state.counterValue % 2 == 0) {
+                  return Text(
+                    'Value is even' + state.counterValue.toString(),
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                }
+                return Text(
+                  state.counterValue.toString(),
+                  style: Theme.of(context).textTheme.headline4,
+                );
+              },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FloatingActionButton(
+                  onPressed: () {
+                    // we need to call the instance of the logic.cubit:
+                    // two ways of doing it:
+                    // BlocProvider.of<CounterCubit>(context).decrement;
+                    // context.bloc<CounterCubit>().decrement();
+                    // Now that we build the BlocProvider we need to change the UI and show how many times
+                    // We pressed the button, for that, we need the BlocBuilder!
+                    // The BlocBuilder is a widget that helps re-building the UI based on the bloc State Changes
+
+                    BlocProvider.of<CounterCubit>(context).decrement;
+                  },
+                  tooltip: 'Decrement',
+                  child: Icon(Icons.remove),
+                ),
+                FloatingActionButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).increment();
+                  },
+                  tooltip: 'Increment',
+                  child: Icon(Icons.add),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            MaterialButton(
+              color: widget.color,
+              onPressed: () {},
+              child: Text('Go to second screen'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
