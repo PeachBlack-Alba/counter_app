ce987a727434eabd5989dbade36aa0398d354b11
PeachBlack-Alba <57803942+PeachBlack-Alba@users.noreply.github.com>
2021-10-16 21:53:23 +0200

    bloc 6.1.0 context.watch()

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -18,140 +18,152 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext homeScreenContext) {
-    return BlocListener<InternetCubit, InternetState>(
-      listener: (context, state) {
-        if (state is InternetConnected &&
-            state.connectionType == ConnectionType.Wifi) {
-          context.bloc<CounterCubit>().increment();
-        } else if (state is InternetConnected &&
-            state.connectionType == ConnectionType.Mobile) {
-          context.bloc<CounterCubit>().decrement();
-        }
-      },
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: widget.color,
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              BlocBuilder<InternetCubit, InternetState>(
-                builder: (context, state) {
-                  if (state is InternetConnected &&
-                      state.connectionType == ConnectionType.Wifi) {
-                    return Text(
-                      'Wi-Fi',
-                      style: Theme.of(context).textTheme.headline3?.copyWith(
-                        color: Colors.green,
-                      ),
-                    );
-                  } else if (state is InternetConnected &&
-                      state.connectionType == ConnectionType.Mobile) {
-                    return Text(
-                      'Mobile',
-                      style: Theme.of(context).textTheme.headline3?.copyWith(
-                        color: Colors.red,
-                      ),
-                    );
-                  } else if (state is InternetDisconnected) {
-                    return Text(
-                      'Disconnected',
-                      style: Theme.of(context).textTheme.headline3?.copyWith(
-                        color: Colors.grey,
-                      ),
-                    );
-                  }
-                  return CircularProgressIndicator();
-                },
-              ),
-              Divider(
-                height: 5,
-              ),
-              BlocConsumer<CounterCubit, CounterState>(
-                listener: (context, state) {
-                  if (state.wasIncremented == true) {
-                    Scaffold.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text('Incremented!'),
-                        duration: Duration(milliseconds: 300),
-                      ),
-                    );
-                  } else if (state.wasIncremented == false) {
-                    Scaffold.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text('Decremented!'),
-                        duration: Duration(milliseconds: 300),
-                      ),
-                    );
-                  }
-                },
-                builder: (context, state) {
-                  if (state.counterValue < 0) {
-                    return Text(
-                      'BRR, NEGATIVE ' + state.counterValue.toString(),
-                      style: Theme.of(context).textTheme.headline4,
-                    );
-                  } else if (state.counterValue % 2 == 0) {
-                    return Text(
-                      'YAAAY ' + state.counterValue.toString(),
-                      style: Theme.of(context).textTheme.headline4,
-                    );
-                  } else if (state.counterValue == 5) {
-                    return Text(
-                      'HMM, NUMBER 5',
-                      style: Theme.of(context).textTheme.headline4,
-                    );
-                  } else {
-                    return Text(
-                      state.counterValue.toString(),
-                      style: Theme.of(context).textTheme.headline4,
-                    );
-                  }
-                },
-              ),
-
-              const SizedBox(
-                height: 24,
-              ),
-              Builder(
-                // Wrapping the widget in a builder so we can specify which
-                // context has to look to, since the MaterialButton context is anonymous
-                // and therefore is not accessible
-                // The look for the context is bottom-up relationship
-                builder: (materialButtonContext) {
-                  return MaterialButton(
-                    color: Colors.redAccent,
-                    child: const Text(
-                      'Go to Second Screen',
-                      style: TextStyle(color: Colors.white),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: widget.color,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            BlocBuilder<InternetCubit, InternetState>(
+              builder: (context, state) {
+                if (state is InternetConnected &&
+                    state.connectionType == ConnectionType.Wifi) {
+                  return Text(
+                    'Wi-Fi',
+                    style: Theme.of(context).textTheme.headline3?.copyWith(
+                      color: Colors.green,
                     ),
-                    onPressed: () {
-                      Navigator.of(materialButtonContext).pushNamed(
-                        '/second',
-                      );
-                    },
+                  );
+                } else if (state is InternetConnected &&
+                    state.connectionType == ConnectionType.Mobile) {
+                  return Text(
+                    'Mobile',
+                    style: Theme.of(context).textTheme.headline3?.copyWith(
+                      color: Colors.red,
+                    ),
+                  );
+                } else if (state is InternetDisconnected) {
+                  return Text(
+                    'Disconnected',
+                    style: Theme.of(context).textTheme.headline3?.copyWith(
+                      color: Colors.grey,
+                    ),
                   );
                 }
-              ),
-              SizedBox(
-                height: 24,
-              ),
-              MaterialButton(
-                color: Colors.greenAccent,
-                child: const Text(
-                  'Go to Third Screen',
-                  style: TextStyle(color: Colors.white),
-                ),
-                onPressed: () {
-                  Navigator.of(homeScreenContext).pushNamed(
-                    '/third',
+                return CircularProgressIndicator();
+              },
+            ),
+            Divider(
+              height: 5,
+            ),
+            BlocConsumer<CounterCubit, CounterState>(
+              listener: (context, state) {
+                if (state.wasIncremented == true) {
+                  Scaffold.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Incremented!'),
+                      duration: Duration(milliseconds: 300),
+                    ),
                   );
-                },
+                } else if (state.wasIncremented == false) {
+                  Scaffold.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Decremented!'),
+                      duration: Duration(milliseconds: 300),
+                    ),
+                  );
+                }
+              },
+              builder: (context, state) {
+                if (state.counterValue < 0) {
+                  return Text(
+                    'BRR, NEGATIVE ' + state.counterValue.toString(),
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                } else if (state.counterValue % 2 == 0) {
+                  return Text(
+                    'YAAAY ' + state.counterValue.toString(),
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                } else if (state.counterValue == 5) {
+                  return Text(
+                    'HMM, NUMBER 5',
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                } else {
+                  return Text(
+                    state.counterValue.toString(),
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                }
+              },
+            ),
+
+            const SizedBox(
+              height: 24,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FloatingActionButton(
+                  heroTag: Text('${widget.title}'),
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).decrement();
+                    // context.bloc<CounterCubit>().decrement();
+                  },
+                  tooltip: 'Decrement',
+                  child: Icon(Icons.remove),
+                ),
+                FloatingActionButton(
+                  heroTag: Text('${widget.title} 2nd'),
+                  onPressed: () {
+                    // BlocProvider.of<CounterCubit>(context).increment();
+                    context.read<CounterCubit>().increment();
+                  },
+                  tooltip: 'Increment',
+                  child: Icon(Icons.add),
+                ),
+              ],
+            ),
+            Builder(
+              // Wrapping the widget in a builder so we can specify which
+              // context has to look to, since the MaterialButton context is anonymous
+              // and therefore is not accessible
+              // The look for the context is bottom-up relationship
+              builder: (materialButtonContext) {
+                return MaterialButton(
+                  color: Colors.redAccent,
+                  child: const Text(
+                    'Go to Second Screen',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () {
+                    Navigator.of(materialButtonContext).pushNamed(
+                      '/second',
+                    );
+                  },
+                );
+              }
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            MaterialButton(
+              color: Colors.greenAccent,
+              child: const Text(
+                'Go to Third Screen',
+                style: TextStyle(color: Colors.white),
               ),
-            ],
-          ),
+              onPressed: () {
+                Navigator.of(homeScreenContext).pushNamed(
+                  '/third',
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
